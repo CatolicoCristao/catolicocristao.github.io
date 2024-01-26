dde5b07fd0ad33fc0a3bfa2dd6d90a881a451c95
JosueSantos <josue.santos@svm.com.br>
2024-01-26 08:52:55 -0300

    feat: add function find data maker

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-
+// Load Map Lib
 let latitude = -3.732708;
 let longitude = -38.525963;
 let zoom = 12;
@@ -8,36 +8,27 @@ const tiles = L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 19,
     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
 }).addTo(map);
+map.on('click', showLocationPosition);
 
-var popup = L.popup();
+const popup = L.popup();
 
-const geolocation = navigator.geolocation;
-geolocation.getCurrentPosition(function(position) {
-    latitude = position.coords.latitude;
-    longitude = position.coords.longitude;
-    zoom = 15;
+// Set Location User
+if (navigator?.userAgentData?.mobile) {
+    const geolocation = navigator.geolocation;
+    geolocation.getCurrentPosition(function (position) {
+        latitude = position.coords.latitude;
+        longitude = position.coords.longitude;
+        zoom = 15;
 
-    map.setView([latitude,longitude], zoom);
-}, function(error) {
-    if (error.code === error.PERMISSION_DENIED) {
-        console.log("O usuário não concedeu permissão para acessar sua localização.");
-    }
-});
-
-function onClickMarker(e) {
-    console.log(e.latlng.lat + ' - ' + e.latlng.lng);
+        map.setView([latitude, longitude], zoom);
+    }, function (error) {
+        if (error.code === error.PERMISSION_DENIED) {
+            console.log("O usuário não concedeu permissão para acessar sua localização.");
+        }
+    });
 }
 
-function onMapClick(e) {
-    document.querySelector('#lat').value = e.latlng.lat + ',' + e.latlng.lng;
-
-    popup.setLatLng(e.latlng)
-        .setContent(`Clique no mapa: ${e.latlng.toString()}`)
-        .openOn(map);
-}
-
-map.on('click', onMapClick);
-
+// Load Data Map
 const url = "https://script.google.com/macros/s/AKfycbw-rCem8tvgz89uy2OOWYnPRy5RN0Ql977cWR-rH2n96v-BCYXXgS3QuFbW0E_RP1o2/exec";
 
 let headers = new Headers();
@@ -51,17 +42,52 @@ const requestBD = fetch(url, headers);
 requestBD.then(res => {
     return res.json();
 }).then(res => {
-    for (const [key, item] of Object.entries(res)) {
-        const marker = L.marker(item.c.split(','))
-            .addTo(map)
-            .bindPopup('<h3>' + item.a + '</h3><p>' + item.b + '.</p>')
-            .on('click', onClickMarker);
+    for (const item of Object.values(res)) {
+        const maker = L.marker(
+            item.c.split(','),
+            {
+                icon: L.icon({
+                    iconUrl: "maker.png",
+                }),
+                title: item.a
+            }
+        );
+        maker.info = item;
+
+        maker.bindPopup(buildPopup(item))
+        maker.on('click', findDataMarker);
+        maker.addTo(map);
     }
 });
 
+function buildPopup(data) {
+    return '<h3>' + data.a + '</h3><p>' + data.b + '.</p>';
+}
+
+function findDataMarker(e) {
+    const nomeIgreja = e.target.info.a;
+    const nomeParoquia = e.target.info.b;
+
+    let htmlBuild = "<h1>" + nomeIgreja + "</h1><h2>" + nomeParoquia + "</h2>"
+
+    document.querySelector('#makerData').innerHTML = htmlBuild;
+
+    document.querySelector('#formContent').style.display = "none";
+    document.querySelector('#makerDataContent').style.display = "block";
+}
+
+function showLocationPosition(e) {
+    document.querySelector('#lat').value = e.latlng.lat + ',' + e.latlng.lng;
+
+    popup.setLatLng(e.latlng)
+        .setContent(`Clique no mapa: ${e.latlng.toString()}`)
+        .openOn(map);
+}
+
+// Form Create Maker
 document.querySelector('button[type="submit"]').addEventListener('click', function (event) {
     event.preventDefault();
-    
+
     const nomeIgreja = document.querySelector('#nome-igreja').value;
     const paroquia = document.querySelector('#paroquia').value;
     const lat = document.querySelector('#lat').value;
@@ -73,7 +99,7 @@ document.querySelector('button[type="submit"]').addEventListener('click', functi
     }));
 
     fetch(url, {
-        mode:  'no-cors',
+        mode: 'no-cors',
         method: 'POST',
         headers: headers,
         body: JSON.stringify({
@@ -83,9 +109,18 @@ document.querySelector('button[type="submit"]').addEventListener('click', functi
         })
     }).then(res => {
         document.querySelector('#alert').innerHTML = nomeIgreja + " adicionada";
-    })
+    });
 
     document.querySelector('#nome-igreja').value = '';
     document.querySelector('#paroquia').value = '';
     document.querySelector('#lat').value = '';
+});
+
+document.addEventListener("DOMContentLoaded", function () {
+    document.querySelectorAll(".collapser-button").forEach(function (button) {
+        button.addEventListener("click", function () {
+            const content = this.nextElementSibling; // Referencia o conteúdo colapsado
+            content.style.display = content.style.display === "none" ? "block" : "none";
+        });
+    });
 });
